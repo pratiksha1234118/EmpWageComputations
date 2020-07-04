1f65f58c5f96074367191146fd6e69319d37be05
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-04 19:48:50 +0530

    Added:functionality check present and absent employee is added

--- empwage.sh
+++ empwage.sh
@@ -1,1 +1,9 @@
 echo "welcome to employee wage computation"
+ispresent=1
+randomCheck=$(( RANDOM % 2 ))
+if [ $ispresent -eq $randomCheck ]
+then
+	echo " employee is present"
+else
+	echo "not present"
+fi

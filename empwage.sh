74cfce3b05d76bf0709b53d12241973358537a71
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-05 18:42:04 +0530

    Added:functionality

--- empwage.sh
+++ empwage.sh
@@ -36,56 +36,5 @@ parttimeemploywage=0
 echo "Wage of $name who does part time is : "$parttimeemploywage
 fi
 
-#uc4
-if [ "$employ" == "Present" ]
-then
-echo "1.Wages of the employ who does full time"
-echo "2.Wages of the employ who does part time"
-echo "3.Exit"
-read -p "Enter your choice from above statements : " ch
 
-case $ch in
-"1")
-	
-	if [ "$employ" == "Present" ]
-	then	
-	Employwageperhour=20
-	worktime=8
-	dailyemploywage=$(($Employwageperhour*$worktime))
-	echo "Wage of $name for full time is : "$dailyemploywage
-	else
-	echo "The employ $name is : " $employ 
-	fi
-;;
-
-"2")
-	
-	
-	if [ "$employ" == "Present" ]
-	then	
-	partworktime=$(($worktime/2))
-	parttimeemploywage=$(($Employwageperhour*$partworktime))
-	echo "Wage of $name for part time is : "$parttimeemploywage
-	else
-	echo "The employ $name is : "$employ
-	fi 
-;;
-
-"3")
-break
-;;
-
-*) 
-echo invalid choice 
-;;
-
-esac
-else
-wage=0
-echo "Wage of $name is : "$wage
-fi
-
-WorkingDaysofMonth=20
-wagesofamonth=$(($dailyemploywage*$WorkingDaysofMonth))
-echo "Wages Per Month of $name is : " $wagesofamonth
 
